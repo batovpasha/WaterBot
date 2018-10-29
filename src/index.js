a360d1c3be7f372f8272c9d61c73cd628ea867bb
batovpasha <batovpasha@gmail.com>
2018-10-29 15:03:43 +0200

    Comment some lines for debugging

--- src/index.js
+++ src/index.js
@@ -270,31 +270,31 @@ bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
                            'Перед вашим числом має стояти слеш "/"');
       
     case '/cashPayment':
-      let order = 'Ваше замовлення:\n';
+      // let order = 'Ваше замовлення:\n';
       
-      for (let i = 0; i < ORDER['bottle'].length; i++)
-        order += `${ORDER['bottle'][i]}, ${ORDER['quantity'][i]} шт.\n`
+      // for (let i = 0; i < ORDER['bottle'].length; i++)
+      //   order += `${ORDER['bottle'][i]}, ${ORDER['quantity'][i]} шт.\n`
 
-      order += `Адреса доставки: ${ORDER['address']}\n` +
-               'Оплата готівкою\n' +
-               '\n' +
-               'Введіть "/ок" для підтвердження або скасування замовлення';
+      // order += `Адреса доставки: ${ORDER['address']}\n` +
+      //          'Оплата готівкою\n' +
+      //          '\n' +
+      //          'Введіть "/ок" для підтвердження або скасування замовлення';
 
-      return say(response, order);
+      // return say(response, order);
       break;     
 
     case '/cashlessPayment':
-      let order = 'Ваше замовлення:\n';
+      // let order = 'Ваше замовлення:\n';
       
-      for (let i = 0; i < ORDER['bottle'].length; i++)
-        order += `${ORDER['bottle'][i]}, ${ORDER['quantity'][i]} шт.\n`
+      // for (let i = 0; i < ORDER['bottle'].length; i++)
+      //   order += `${ORDER['bottle'][i]}, ${ORDER['quantity'][i]} шт.\n`
 
-      order += `Адреса доставки: ${ORDER['address']}\n` +
-               'Безготівковий розрахунок\n' +
-               '\n' +
-               'Введіть "/ок" для підтвердження або скасування замовлення';
+      // order += `Адреса доставки: ${ORDER['address']}\n` +
+      //          'Безготівковий розрахунок\n' +
+      //          '\n' +
+      //          'Введіть "/ок" для підтвердження або скасування замовлення';
 
-      return say(response, order);     
+      // return say(response, order);     
       break;
 
     case '/ок':
