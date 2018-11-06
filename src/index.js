2704e6c1b91e39a1290b86c293f9f87eadb05023
batovpasha <batovpasha@gmail.com>
2018-11-06 12:25:53 +0200

    Fixed shielding for regex

--- src/index.js
+++ src/index.js
@@ -214,7 +214,7 @@ bot.onTextMessage(/\/[1-9][0-9]*/, (message, response) => {
                      + 'Приклад: <вул. Бажана, 42, кв. 20>');
 });
 
-bot.onTextMessage(/<.*>/, (message, response) => {
+bot.onTextMessage(/\<.*\>/, (message, response) => {
   if (ORDER['address']) return;
   
   ORDER['address'] = message.text.match(/[^<].*[^>]/).join(''); // value without ""
