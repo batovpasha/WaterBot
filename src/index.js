71e6c69467ffa79de83f5f7a51bbfd7c89b55c7f
batovpasha <batovpasha@gmail.com>
2018-11-07 14:45:39 +0200

    Wrote correct output for cashlessPayment case

--- src/index.js
+++ src/index.js
@@ -345,14 +345,17 @@ bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
       cashlessOrder += `Адреса доставки: ${ORDER['address']}\n ` +
                        'Безготівковий розрахунок\n ' +
                        `Вартість: ${cashlessPrice} грн\n ` +
-                       'Введіть "/ок" для підтвердження або скасування замовлення';
+                       'Будь ласка, перейдіть за посиланням та оплатіть товар';
     
                        
       const cashlessOrderForUrl = cashlessOrder.split(' ').join('%20');
                        
       // PAY_KEYBOARD['Buttons'][0]['ActionBody'] = `https://api.fondy.eu/api/checkout?button=%7B"merchant_id"%3A1415599%2C"currency"%3A"UAH"%2C"fields"%3A%5B%7B"name"%3A"id-adpgQ8AFYf"%2C"label"%3A"Коментар%20до%20замовлення%3A"%2C"valid"%3A"max_length%3A1000%3B"%7D%5D%2C"params"%3A%7B"response_url"%3A"%7Bresponse_url%7D"%2C"lang"%3A"uk"%2C"order_desc"%3A""%7D%2C"amount"%3A"${cashlessPrice.toString()}"%2C"amount_readonly"%3Atrue%7D`;
                        
-      return say(response, `https://api.fondy.eu/api/checkout?button=%7B"merchant_id"%3A1415599%2C"currency"%3A"UAH"%2C"fields"%3A%5B%7B"name"%3A"id-adpgQ8AFYf"%2C"label"%3A"Коментар%20до%20замовлення%3A"%2C"valid"%3A"max_length%3A1000%3B"%7D%5D%2C"params"%3A%7B"response_url"%3A"%7Bresponse_url%7D"%2C"lang"%3A"uk"%2C"order_desc"%3A""%7D%2C"amount"%3A"${cashlessPrice.toString()}"%2C"amount_readonly"%3Atrue%7D`);
+      say(response, cashlessOrder);
+      say(response, `https://api.fondy.eu/api/checkout?button=%7B"merchant_id"%3A1415599%2C"currency"%3A"UAH"%2C"fields"%3A%5B%7B"name"%3A"id-adpgQ8AFYf"%2C"label"%3A"Коментар%20до%20замовлення%3A"%2C"valid"%3A"max_length%3A1000%3B"%7D%5D%2C"params"%3A%7B"response_url"%3A"%7Bresponse_url%7D"%2C"lang"%3A"uk"%2C"order_desc"%3A""%7D%2C"amount"%3A"${cashlessPrice.toString()}"%2C"amount_readonly"%3Atrue%7D`);
+      say(response, 'Введіть "/ок" для підтвердження або скасування замовлення');
+      
       break;
 
     case '/ок':
